64d462badd9b979c4b31a81d1528c02a7e18b38f
unknown <utahranger3@gmail.com>
2023-03-02 10:03:06 -0700

    copy pipeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,22 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
+                // Get some code from a GitHub repository
+                git 'https://github.com/utahranger3/spring-petclinic.git'
+            }
         }
         stage('Build') {
             steps {
                 sh './mvnw clean package'
-                //sh 'false' // true
             }
-        
+
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
